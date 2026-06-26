6b91d44769e6345c9ee4efbc3e1e0298d1627f0c
Ben Cohen <benxcohen@gmail.com>
2026-06-27 00:06:47 +0300

    Header docs

--- allocator.h
+++ allocator.h
@@ -38,14 +38,26 @@ typedef struct
     size_t avail;
 } heapinfo;
 
+// Main allocator function, takes a size and returns a curated memory chunk
+void *balloc(size_t size);
+// Frees the memory chunk and adds it back to the freelist, while verifying chunk's integrity
+void bfree(void *memory);
+
+// Initializes a new heap with 1 memory page
 int init_heap(heapinfo *heap);
 
-void *balloc(size_t size);
-void bfree(void *memory);
+// Searches for free chunks from the bins in O(1) at the average case
+void find_free_chunk(heapchunk **output_ptr, size_t size);
+
+// Calculates and classefies required bin based on chunk's size
 int get_bin_index(size_t size);
+
+// Splits one big chunk to save the unnecessary memory
 void split_chunk(heapchunk *avail_chunk, size_t requested_size);
+
+// Asks the OS for more memory when the heap runs out
 int increase_heap(heapchunk **output_ptr, size_t required_space);
 void *request_space(size_t required_space);
-void find_free_chunk(heapchunk **output_ptr, size_t size);
+
 void add_to_bin(heapchunk *chunk);
 void remove_from_bin(heapchunk *chunk);
